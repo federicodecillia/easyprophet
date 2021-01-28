34ed1cefed6a8774147d74f0c22100dff8266aed
Federico De Cillia <federicodecillia@users.noreply.github.com>
2021-01-28 10:45:55 +0100

    docs: added CHANGELOG.md

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="easyprophet",  # directory name right below src/
-    version="1.0.0",
+    version="1.0.1",
     author="Federico De Cillia",
     description="Python package developed to simplify Prophet application to any data.",
     long_description=readme,
