ee1087201ec6ebc38736b52a0cd66cfa637424d1
Federico De Cillia <federico.decillia@luxottica.com>
2021-09-21 14:43:36 +0200

    fix: 1.1.2 Minor fixes

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "pandas",
         "plotly",
         "scikit-learn",
-        "scipyf",
+        "scipy",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
